ae0c564a803f01d0dd1bb1d1ecd4ee0739b01270
johhny47 <mdjohnny063@gmail.com>
2024-12-08 17:42:56 +0600

    add BannerSec

--- src/component/Banner/BannerSec.jsx
+++ src/component/Banner/BannerSec.jsx
@@ -2,8 +2,8 @@
 
 const BannerSec = () => {
     return (
-        <div  className="py-20  text-black lg:flex items-center justify-around">
-        
+        <div  className="  text-black lg:flex items-center justify-around">
+        <img src="https://i.ibb.co.com/5MZW3HG/DALL-E-2024-12-07-21-43-50-A-dynamic-banner-image-for-a-sports-equipment-shop-showcasing-a-close-up.webp"  className="h-[580px]" alt="" />
             
         </div>
     );
